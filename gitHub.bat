0bee1de0696e15d0b6c3be972cc14ca8dc7b9aac
MAJ123mf <maj.capelnik@gmail.com>
2025-02-23 10:39:15 +0100

    Finishing version

--- gitHub.bat
+++ gitHub.bat
@@ -4,7 +4,8 @@ git add C:\pythonProjects\pointOperations\point3d.py
 git add C:\pythonProjects\pointOperations\point2dListOperations.py
 git add C:\pythonProjects\pointOperations\pointOperations.py
 git add C:\pythonProjects\pointOperations\test.py
+git add C:\pythonProjects\pointOperations\site-packages.zip
 git add C:\pythonProjects\pointOperations\__init__.py
 git commit -am "Finishing version"
 git remote add origin https://github.com/MAJ123mf/pointOperations.git
-git push origin master
+git push origin main
